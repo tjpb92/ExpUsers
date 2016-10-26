27f9e271efcf46700db778ec1a0db52b665e318c
Thierry Baribaud <thierry.baribaud@gmail.com>
2016-10-27 00:11:40 +0200

    Evolutions au 26/10/2016
    - Modification de la classe ExpUsers.java : premier lecture de la base mongoDb

--- ExpUsers/src/expusers/ExpUsers.java
+++ ExpUsers/src/expusers/ExpUsers.java
@@ -1,5 +1,10 @@
 package expusers;
 
+import bkgpi2a.User;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -14,6 +19,7 @@ import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.bson.Document;
 
 /**
  * Programme pour exporter les utilisateurs d'un site Web dans un fichier Excel
@@ -24,7 +30,10 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ExpUsers {
 
     private final static String filename = "users.xlsx";
-    private static Object XHSSFCellStyle;
+//    private static Object XHSSFCellStyle;
+
+    private final static String HOST = "192.168.0.17";
+    private final static int PORT = 27017;
 
     /**
      * @param args arguments en ligne de commande
@@ -39,7 +48,29 @@ public class ExpUsers {
         XSSFRow ligne = null;
         XSSFCellStyle blackMediumBorder = null;
 
-        // Création d'un classeur Excel
+        MongoDatabase MyDatabase;
+
+        MongoClient MyMongoClient = new MongoClient(HOST, PORT);
+
+        System.out.println("Liste des bases de données :");
+        for (String MyDbNname : MyMongoClient.listDatabaseNames()) {
+            System.out.println("  " + MyDbNname);
+            MyDatabase = MyMongoClient.getDatabase(MyDbNname);
+            System.out.println("Liste des collections de " + MyDbNname + " :");
+            for (String MyCollectionName : MyDatabase.listCollectionNames()) {
+                System.out.println("  " + MyCollectionName);
+            }
+        }
+
+        MyDatabase = MyMongoClient.getDatabase("extranet");
+        MongoCollection<Document> MyCollection = MyDatabase.getCollection("users");
+        System.out.println(MyCollection.count() + " utilisateurs");
+
+        Document MyDocument = MyCollection.find().first();
+        System.out.println(MyDocument.toJson());
+
+        
+//      Création d'un classeur Excel
         classeur = new XSSFWorkbook();
         feuille = classeur.createSheet("Utilisateurs");
         titre = feuille.createRow(0);
@@ -77,15 +108,61 @@ public class ExpUsers {
         cell.setCellStyle(blackMediumBorder);
         cell.setCellValue("Société");
 
-        // Initialisation tableau
-        for (int i = 0; i < 10; i++) {
-            ligne = feuille.createRow(i + 1);
-            for (int j = 0; j < 5; j++) {
-                cell = ligne.createCell((short) j);
-                cell.setCellValue(i * j);
+//        MongoCursor<User> cursor = MyCollection.find(User.class).iterator();
+//        try {
+//            while (cursor.hasNext()) {
+//                System.out.println(cursor.next());
+//            }
+//        } finally {
+//            cursor.close();
+//        }
+        Document user;
+        Document company;
+        MongoCursor<Document> MyCursor = MyCollection.find().iterator();
+        int n=0;
+        try {
+            while (MyCursor.hasNext()) {
+                user = MyCursor.next();
+//                company = user.get(company);
+                System.out.println("lastName:" + user.getString("lastName") + 
+                        ", firstName:" + user.getString("firstName") +
+//                        ", userType:" + user.getString("userType") + 
+//                        ", status:" + user.getBoolean("isActive") +
+//                        ", company:" + (Document) (user.get("company")).getString("label")) + 
+                        ", login:" + user.getString("login"));
+//                System.out.println(MyCursor.next().toJson());
+                n++;
+                ligne = feuille.createRow(n);
+    
+                cell = ligne.createCell(0);
+                cell.setCellValue(user.getString("lastName"));
+                cell.setCellStyle(blackMediumBorder);
+
+                cell = ligne.createCell(1);
+                cell.setCellValue(user.getString("firstName"));
+                cell.setCellStyle(blackMediumBorder);
+
+                cell = ligne.createCell(2);
+                cell.setCellValue(user.getString("userType"));
+                cell.setCellStyle(blackMediumBorder);
+
+                cell = ligne.createCell(3);
+                cell.setCellValue(user.getString("login"));
                 cell.setCellStyle(blackMediumBorder);
             }
+        } finally {
+            MyCursor.close();
         }
+        
+//         Initialisation tableau
+//        for (int i = 0; i < 10; i++) {
+//            ligne = feuille.createRow(i + 1);
+//            for (int j = 0; j < 5; j++) {
+//                cell = ligne.createCell((short) j);
+//                cell.setCellValue(i * j);
+//                cell.setCellStyle(blackMediumBorder);
+//            }
+//        }
 
         // Enregistrement du classeur dans un fichier
         try {
